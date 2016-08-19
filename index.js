713ce37405de3bbd65cada18ec355f8a5b9ce41d
ansteh <stehle.andre@googlemail.com>
2016-08-19 23:30:41 +0200

    files package.json

--- index.js
+++ index.js
@@ -1,60 +1,6 @@
 'use strict';
 const _ = require('lodash');
 
-const horizontal = (json, collector) => {
-  if(_.isUndefined(collector)) {
-    collector = {};
-  }
-
-  _.forOwn(json, (value, key) => {
-    if(isNativeType(value)) {
-      collector[key] = value;
-    } else if(_.isArray(value)) {
-      let parsed = parseArrayHorizontally(value, key);
-      _.assign(collector, parsed);
-    } else {
-      let parsed = horizontal(value);
-      parsed = extendKeyNamesByPrefix(key, parsed)
-      _.assign(collector, parsed);
-    }
-  });
-
-  return collector;
-};
-
-const isNativeType = (value) => {
-  return _.isString(value) || _.isNumber(value);
-};
-
-const extendKeyNamesByPrefix = (prefix, json) => {
-  return _.forOwn(json, (value, key) => {
-    delete json[key];
-    json[`${prefix}.${key}`] = value;
-  });
-};
-
-const parseArrayHorizontally = (collection, prefix) => {
-  let collector = {};
-
-  _.forEach(collection, (value, index) => {
-    let key = `${prefix}.${index}`;
-
-    if(isNativeType(value)) {
-      collector[key] = value;
-    } else if(_.isArray(value)) {
-      let parsed = parseArrayHorizontally(value, key);
-      _.assign(collector, parsed);
-    } else {
-      let parsed = horizontal(value);
-      parsed = extendKeyNamesByPrefix(key, parsed)
-      _.assign(collector, parsed);
-    }
-
-  });
-
-  return collector;
-};
-
 const vertical = (json) => {
   let head = {};
   let parts = [];
@@ -114,7 +60,18 @@ const partition = (json) => {
   return groups;
 };
 
+const isNativeType = (value) => {
+  return _.isString(value) || _.isNumber(value);
+};
+
+const extendKeyNamesByPrefix = (prefix, json) => {
+  return _.forOwn(json, (value, key) => {
+    delete json[key];
+    json[`${prefix}.${key}`] = value;
+  });
+};
+
 module.exports = {
-  horizontal: horizontal,
+  horizontal: require('./lib/horizontal.js'),
   vertical: vertical
 };
